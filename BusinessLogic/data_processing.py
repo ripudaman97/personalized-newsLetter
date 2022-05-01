e17c60ad5e577bc6ef37be7eea0a97481b621dd0
Ripudaman Singh Uppal <43295539+ripudaman97@users.noreply.github.com>
2022-05-01 11:32:59 -0400

    normalizing data

---
+++ BusinessLogic/data_processing.py
@@ -0,0 +1,19 @@
+
+from sklearn import preprocessing
+import pandas as pd
+
+
+# loading data
+credit_data = pd.read_csv ("../raw_data.csv")
+
+# normalizing and scaling data
+
+normalized_credit_data = preprocessing.MinMaxScaler()
+col_names = credit_data.columns
+d = normalized_credit_data.fit_transform(credit_data)
+
+scaled_credit_data = pd.DataFrame(d,columns=col_names)
+
+#print(scaled_credit_data)
+
+
